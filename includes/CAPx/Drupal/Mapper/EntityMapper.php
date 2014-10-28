b0b6aabc895e6eb96f579b0c6f0c2b454c0558b3
RoSk0 <roskoliy.kirill@gmail.com>
2014-10-28 22:11:32 +0300

    CAPX-57: Added logging and user notification for situations when mapped field was removed from bundle/system.

--- includes/CAPx/Drupal/Mapper/EntityMapper.php
+++ includes/CAPx/Drupal/Mapper/EntityMapper.php
@@ -5,9 +5,11 @@
  */
 
 namespace CAPx\Drupal\Mapper;
+
 use CAPx\Drupal\Processors\FieldProcessors\FieldProcessor;
 use CAPx\Drupal\Processors\PropertyProcessors\PropertyProcessor;
 use CAPx\Drupal\Processors\FieldCollectionProcessor;
+use CAPx\Drupal\Util\CAPxMapper;
 
 class EntityMapper extends MapperAbstract {
 
@@ -54,62 +56,68 @@ class EntityMapper extends MapperAbstract {
 
     // Loop through each field and run a field processor on it.
     foreach ($config['fields'] as $fieldName => $remoteDataPaths) {
-      $info = array();
-
-      drupal_alter('capx_pre_map_field', $entity, $fieldName, $remoteDataPaths);
-
-      // Allow just one path as a string.
-      if (!is_array($remoteDataPaths)) {
-        $remoteDataPaths = array($remoteDataPaths);
-      }
-
-      // Loop through each of the data paths.
-      foreach ($remoteDataPaths as $key => $dataPath) {
-
-        // No setting was provided.
-        if (empty($dataPath)) {
-          continue;
-        }
-
-        // Attempt to get the data based on the path that was provided.
-        // No guarentee that the user wont enter valid jsonpath notation that
-        // does not have a valid result.
-        try {
-          $info[$key] = $this->getRemoteDataByJsonPath($data, $dataPath);
-        } catch(\Exception $e) {
-          // ... silently continue. Please dont shoot me.
-          // @todo: Add debugging logs/help here.
-          continue;
-        }
-      }
-
-      // We got nothing!
-      if (empty($info)) {
-        // @todo: log this
-        continue;
-      }
-
       // Get some information about the field we are going to process.
-      $fieldInfoInstance = field_info_instance($entity->type(), $fieldName, $entity->getBundle());
       $fieldInfoField = field_info_field($fieldName);
+      if ($fieldInfoField) {
+        $fieldInfoInstance = field_info_instance($entity->type(), $fieldName, $entity->getBundle());
+        if ($fieldInfoInstance) {
+          $info = array();
 
-      // Widgets can change the way the data needs to be parsed. Provide that
-      // to the FieldProcessor.
-      $widget = $fieldInfoInstance['widget']['type'];
-      $field = $fieldInfoField['type'];
+          drupal_alter('capx_pre_map_field', $entity, $fieldName, $remoteDataPaths);
 
-      // Create a new field processor and let it do its magic.
-      $fieldProcessor = new FieldProcessor($entity, $fieldName);
-      $fieldProcessor->field($field)->widget($widget)->put($info);
+          // Allow just one path as a string.
+          if (!is_array($remoteDataPaths)) {
+            $remoteDataPaths = array($remoteDataPaths);
+          }
 
-      // Allow altering of an entity after this process.
-      drupal_alter('capx_post_map_field', $entity, $fieldName);
+          // Loop through each of the data paths.
+          foreach ($remoteDataPaths as $key => $dataPath) {
 
+            // No setting was provided.
+            if (empty($dataPath)) {
+              continue;
+            }
+
+            // Attempt to get the data based on the path that was provided.
+            // No guarentee that the user wont enter valid jsonpath notation that
+            // does not have a valid result.
+            try {
+              $info[$key] = $this->getRemoteDataByJsonPath($data, $dataPath);
+            }
+            catch (\Exception $e) {
+              $message = 'There was an exception when trying to get data by @path. Exception message is: @message.';
+              $message_vars = array(
+                '@path' => $dataPath,
+                '@message' => $e->getMessage(),
+              );
+              watchdog('stanford_capx_jsonpath', $message, $message_vars);
+              continue;
+            }
+          }
+
+          // We got nothing!
+          if (empty($info)) {
+            // @todo: log this
+            continue;
+          }
+
+          // Widgets can change the way the data needs to be parsed. Provide that
+          // to the FieldProcessor.
+          $widget = $fieldInfoInstance['widget']['type'];
+          $field = $fieldInfoField['type'];
+
+          // Create a new field processor and let it do its magic.
+          $fieldProcessor = new FieldProcessor($entity, $fieldName);
+          $fieldProcessor->field($field)->widget($widget)->put($info);
+
+          // Allow altering of an entity after this process.
+          drupal_alter('capx_post_map_field', $entity, $fieldName);
+        }
+      }
     }
 
     // Set the entity again for changes.
     $this->setEntity($entity);
-
   }
 
   /**
@@ -196,5 +204,71 @@ class EntityMapper extends MapperAbstract {
     $this->setEntity($entity);
   }
 
+  /**
+   * Checks that fields used in this mapper still in place.
+   *
+   * If it happens that field used in mapper will be removed from bundle
+   * or from the system entirely this will put a watchdog message and put a
+   * persistent message for admin users on admin pages.
+   */
+  public function checkFields() {
+    $config = $this->getConfig();
+    $entity_type = $config['entity_type'];
+    $bundle = $config['bundle_type'];
+    $fields = $config['fields'];
 
+    foreach (array_keys($fields) as $fieldName) {
+      $fieldInfoField = field_info_field($fieldName);
+      if ($fieldInfoField) {
+        $fieldInfoInstance = field_info_instance($entity_type, $fieldName, $bundle);
+        if (!$fieldInfoInstance) {
+          // Field was removed from this bundle.
+          $messages = variable_get('stanford_capx_field_issues', array());
+          $mapper = $this->getMapper();
+          $message_key = $mapper->getMachineName() . ':' . $entity_type . ':' . $bundle . ':' . $fieldName;
+
+          if (empty($messages[$message_key])) {
+            $message_vars = array(
+              '%field' => $fieldName,
+              '%entity_type' => $entity_type,
+              '%bundle' => $bundle,
+              '!mapper' => l(check_plain($mapper->label()), 'admin/config/capx/mapper/edit/' . $mapper->getMachineName()),
+            );
+            $message = t('Field %field was removed from the %entity_type bundle %bundle, but is still used in !mapper. You should check configuration of the specified mapper!');
+            watchdog('stanford_capx_field_issues', $message, $message_vars, WATCHDOG_ERROR);
+            $messages[$message_key] = array(
+              'text' => $message,
+              'message_vars' => $message_vars,
+            );
+            variable_set('stanford_capx_field_issues', $messages);
+          }
+        }
+      }
+      else {
+        // Field was removed from system.
+        $messages = variable_get('stanford_capx_field_issues', array());
+        $message_key = $fieldName;
+
+        if (empty($messages[$message_key])) {
+          $mappers = CAPxMapper::loadAllMappers();
+          // Filtering mappers that uses this field.
+          $mapper_links = array();
+          foreach ($mappers as $mapper) {
+            if (array_key_exists($fieldName, $mapper->fields)) {
+              $mapper_links[$mapper->getMachineName()] = l(check_plain($mapper->label()), 'admin/config/capx/mapper/edit/' . $mapper->getMachineName());
+            }
+          }
+          $message_vars = array('%field' => $fieldName, '!mappers' => implode(', ', $mapper_links));
+          $message = t('Field %field was removed from the system, but is still used in !mappers. You should check configuration of the specified mappers!');
+          watchdog('stanford_capx_field_issues', $message, $message_vars, WATCHDOG_ERROR);
+          $messages[$message_key] = array(
+            'text' => $message,
+            'message_vars' => $message_vars,
+            'mappers' => $mapper_links,
+          );
+          variable_set('stanford_capx_field_issues', $messages);
+        }
+      }
+    }
+  }
 }
