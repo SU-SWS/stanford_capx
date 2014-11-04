dbedee93bbf7937bc7dc967ab44847cbbe3fb5d4
RoSk0 <roskoliy.kirill@gmail.com>
2014-11-04 19:39:39 +0300

    CAPX-76: Added trimming of text fields to maximum allowed length.

--- includes/CAPx/Drupal/Processors/FieldProcessors/TextFieldFieldProcessor.php
+++ includes/CAPx/Drupal/Processors/FieldProcessors/TextFieldFieldProcessor.php
@@ -9,12 +9,38 @@ namespace CAPx\Drupal\Processors\FieldProcessors;
 class TextFieldFieldProcessor extends FieldTypeProcessor {
 
   /**
-   * Default implementation of put
-   * @see  parent::put();
+   * Default implementation of put.
+   *
+   * @see FieldProcessorAbstract::put()
    */
   public function put($data) {
+    $data = $this->prepareData($data);
+
     parent::put($data);
   }
 
+  /**
+   * Prepares CAP API data to feet to Drupal field.
+   *
+   * @param array $data
+   *   CAP API field data.
+   *
+   * @return array
+   *   Prepared data.
+   */
+  public function prepareData($data) {
+    $fieldName = $this->getFieldName();
+    $fieldInfo = field_info_field($fieldName);
+    $maxLength = $fieldInfo['settings']['max_length'];
+
+    foreach ($data as $column => $values) {
+      foreach ($values as $delta => $value) {
+        // @todo: Should we log this? Keep in mind that log will be polluted.
+        $data[$column][$delta] = drupal_substr($value, 0, $maxLength);
+      }
+    }
+
+    return $data;
+  }
 
 }
