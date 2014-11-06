b89ce6060b1b2dcdfa17b546376f40742447e811
RoSk0 <roskoliy.kirill@gmail.com>
2014-11-06 15:50:48 +0300

    CAPX-71: Improved file and image field processors.

--- includes/CAPx/Drupal/Processors/FieldProcessors/FileFieldProcessor.php
+++ includes/CAPx/Drupal/Processors/FieldProcessors/FileFieldProcessor.php
@@ -8,170 +8,225 @@ namespace CAPx\Drupal\Processors\FieldProcessors;
 
 class FileFieldProcessor extends FieldTypeProcessor {
 
-  // The path to save the file to.
-  // @todo: Make this a Drupal configuration setting.
-  protected $saveDir = "public://capx/profile-files/";
-
   /**
    * Override the default put implementation because file needs special things.
+   *
    * The file put needs an array of information per entry. This is ok because
    * the CAP API provides it in a way we can use.
-   * @param   array $data [description]
-   *                $data['contentType']  = "image/jpeg"
-   *                $data['placeholder']  = false
-   *                $data['type']         = "big"
-   *                $data['url']          = "https://...."
+   *
+   * @param array $data
+   *   Fetched remote data. There is two required entries in this array:
+   * - contentType - Describing file type
+   * - url - URL to fetch file
+   *
+   * @see FieldProcessorAbstract::put()
    */
   public function put($data) {
+    $data = $this->process($data);
 
-    // Normalize data because it comes in a bit funky as we take whole array
-    // from the CAP API data.
-    $data = $data[0];
-
-    if (!is_array($data)) {
-      throw new \Exception("FileFieldProcessor Requires Data to be an array");
-    }
-
-    // Loop through each and ensure that the values are there.
-    foreach ($data as $index => $values) {
-      // Validate we have our data
-      if (!isset($values['contentType']) || !isset($values['url'])) {
-        throw new \Exception("Missing required information for field processor.");
-      }
-
-      // Ok, enough validation lets make bacon.
-      $this->process($values);
-    }
-
+    parent::put($data);
   }
 
   /**
+   * Process incoming data.
+   *
    * The meat function of this processor. Take the data and turn it into a file.
    * This function will fetch the remote file and save it to the file system.
-   * @todo : Put a check in place to see if the file has changed. Waiting on the
-   * cap api to provide a 'last updated' timestamp on the file itself.
-   * @param  array $data any array of information from the CAP API
-   * @return [type]         [description]
+   *
+   * @param array $data
+   *   An array of information from the CAP API.
+   *
+   * @return array
+   *   Data that feets to Drupal.
    */
   public function process($data) {
-
-    $entity = $this->getEntity();
-    $fieldName = $this->getFieldName();
-    $fieldInfo = field_info_field($fieldName);
-
-    // Allow altering as this could get messy.
-    drupal_alter('capx_pre_fetch_remote_file', $data);
-
-    // Request the file from the remote server.
-    $response = $this->fetchRemoteFile($data);
-
-    // Save the file.
-    $filename = $this->getFileName($data);
-    $body = $response->getBody();
-    $file = file_save_data($body, $filename, FILE_EXISTS_REPLACE);
-
-    if (!$file) {
-      watchdog('stanford_capx', 'Could not save file: ' . $data['url'], array(), WATCHDOG_DEBUG);
-      return;
+    $return = array();
+    // Normalize data because it comes in a bit funky as we take whole array
+    // from the CAP API data.
+    $data = reset($data);
+    if (!is_array($data)) {
+      $this->logIssue(new \Exception(t('FileFieldProcessor Requires Data to be an array')));
+      return array();
     }
 
-    // We have a file, allow more altering.
-    drupal_alter('capx_post_save_remote_file', $file, $filename);
+    foreach ($data as $values) {
+      foreach ($values as $value) {
+        // Validate we have required data.
+        if (empty($value['contentType']) || empty($value['url'])) {
+          $this->logIssue(new \Exception(t('Missing required information for field processor.')));
+          continue;
+        }
 
-    // All went well go and save it.
-    $setData = array(
-      'fid' => $file->fid,
-      'file' => $file,
-      'display' => variable_get('capx_display_file_default', 1),
-    );
+        // Allow altering as this could get messy.
+        drupal_alter('capx_pre_fetch_remote_file', $value);
 
-    // Check cardinality.
-    if ($fieldInfo['cardinality'] !== "1") {
-      // wrap the setData in another array.
-      $setData = array($setData);
+        // @todo We can put a check in place to see if file was changed so
+        // we don't fetch it again, but Drupal doesn't allow to store
+        // lastModified data by default, this means we will need to handle
+        // this ourselves. DO we really want to do it?
+        // Request the file from the remote server.
+        $file_data = $this->fetchRemoteFile($value);
+        if (empty($file_data)) {
+          continue;
+        }
+
+        $filename = $this->getFilePath($value);
+        if (!$filename) {
+          continue;
+        }
+
+        $file = file_save_data($file_data, $filename, FILE_EXISTS_REPLACE);
+
+        if (!$file) {
+          $this->logIssue(new \Exception(t('Could not save file with filename %name from URL @url', array('%name' => $filename, '@url' => $value['url']))));
+          continue;
+        }
+
+        // We have a file, allow more altering.
+        drupal_alter('capx_post_save_remote_file', $file, $filename);
+
+        // @todo This Place is good candidate to become a new method, which can be overridden in image.
+        $fieldName = $this->getFieldName();
+        $fieldInfo = field_info_field($fieldName);
+        $entityType = $this->getEntity()->type();
+        $bundle = $this->getEntity()->getBundle();
+        $fieldInstance = field_info_instance($entityType, $fieldName, $bundle);
+
+        if ($fieldInfo['type'] == 'file') {
+          $display = TRUE;
+          if (!empty($fieldInfo['settings']['display_field'])) {
+            $display = (bool) $fieldInfo['settings']['display_default'];
+          }
+
+          $description = '';
+          if (!empty($fieldInstance['settings']['description_field']) && isset($value['label']) && !empty($value['label']['text'])) {
+            $description = $value['label']['text'];
+          }
+
+          $return['description'][] = $description;
+          $return['display'][] = $display;
+        }
+
+        $return['fid'][] = $file->fid;
+      }
     }
 
-    // Set the thing.
-    $entity->{$fieldName}->set($setData);
+    return $return;
   }
 
   /**
    * Fetches a remote file from the CAP API servers.
-   * @param  array $data an array of information needed to fetch a file from
+   *
+   * @param array $data
+   *   An array of information needed to fetch a file from
    * the CAP API servers
-   * @return Response         Guzzle response object
+   *
+   * @return string
+   *   Retrieved file data.
    */
   public function fetchRemoteFile($data) {
+    $file = NULL;
     // Fetch the image from CAP.
     $client = new \CAPx\APILib\HTTPClient();
     $guzzle = $client->getHttpClient();
-    $response = $guzzle->get($data['url'])->send();
-
-    if ($response->getStatusCode() !== 200) {
-      throw new \Exception("Could not fetch file: " . $data['url']);
+    try {
+      $response = $guzzle->get($data['url'])->send();
+    }
+    catch (\Exception $e) {
+      $this->logIssue($e);
     }
 
-    return $response;
+    if ($response->getStatusCode() !== 200) {
+      $this->logIssue(new \Exception(t('Could not fetch file from URL: @url', array('@url' => $data['url']))));
+    }
+    else {
+      $file = $response->getBody(TRUE);
+    }
+
+    return $file;
   }
 
   /**
-   * Gets the file name for the remote file by appending the appropriate
-   * file extension to it.
-   * @param  [type] $data [description]
-   * @return string       a unique filename
+   * Gets the file name for the remote file.
+   *
+   * Appending the appropriate file extension to it.
+   *
+   * @param array $data
+   *   File data.
+   *
+   * @return string
+   *   A unique filename.
    */
   public function getFileName($data) {
 
-    $saveDir = $this->getSaveDir();
     $extension = $this->getExtentionByType($data['contentType']);
-    file_prepare_directory($saveDir, FILE_CREATE_DIRECTORY);
-    $filename = $saveDir . md5($data['url']) . $extension;
+    $filename = md5($data['url']) . $extension;
 
     return $filename;
   }
 
 
   /**
-   * The file type is provided by the CAP api but a file extention is not. Here
+   * Returns file extension based on MIME type.
+   *
+   * The file type is provided by the CAP api but a file extension is not. Here
    * we match them up.
-   * @param  string $type the type of file being saved.
-   * @return string       the matching extension with leading period.
+   *
+   * @param string $type
+   *   The type of file being saved.
+   *
+   * @return string
+   *   The matching extension with leading period.
    */
   public function getExtentionByType($type) {
+    include_once DRUPAL_ROOT . '/includes/file.mimetypes.inc';
+    $mapping = file_mimetype_mapping();
+    $mimetypes = array_flip($mapping['mimetypes']);
+    $extensions = array_flip($mapping['extensions']);
 
-    if (empty($type)) {
-      return false;
-    }
-     switch ($type) {
-       case 'image/bmp': return '.bmp';
-       case 'image/gif': return '.gif';
-       case 'image/jpeg': return '.jpg';
-       case 'image/tiff': return '.tif';
-       case 'image/x-icon': return '.ico';
-       case 'image/x-rgb': return '.rgb';
-       case 'image/png': return '.png';
-       case 'image/x-jps': return '.jps';
-       default: return false;
-      // TODO: https://github.com/EllisLab/CodeIgniter/blob/develop/application/config/mimes.php
-     }
+    $extension = isset($mimetypes[$type]) ? $extensions[$mimetypes[$type]] : 'unknown';
 
+    return '.' . $extension;
   }
 
   /**
-   * Getter function
-   * @return string the destination to save the file. PUBLIC.
+   * Returns directory to store current file.
+   *
+   * @return string
+   *   The destination to save the file.
    */
   public function getSaveDir() {
-    return $this->saveDir;
+    $fieldName = $this->getFieldName();
+    $fieldInfo = field_info_field($fieldName);
+    $entityType = $this->getEntity()->type();
+    $bundle = $this->getEntity()->getBundle();
+    $fieldInstance = field_info_instance($entityType, $fieldName, $bundle);
+
+    $schema = $fieldInfo['settings']['uri_scheme'] . '://';
+    $fileDirectory = $fieldInstance['settings']['file_directory'];
+    $fileDirectory = empty($fileDirectory) ? $schema : $schema . $fileDirectory . '/';
+
+    if (!file_prepare_directory($fileDirectory, FILE_CREATE_DIRECTORY)) {
+      $this->logIssue(new \Exception(t('Cannot create directory %directory to store fetched file.', array('%directory' => $fileDirectory))));
+      $fileDirectory = NULL;
+    }
+
+    return $fileDirectory;
   }
 
   /**
-   * Setter function
-   * @param string $dir the destination to save the file. PUBLIC.
+   * Returns file path.
+   *
+   * Includes access scheme and filename.
+   *
+   * @param array $data
+   *   File data.
+   *
+   * @return string
+   *   File path.
    */
-  public function setSaveDir($dir) {
-    $this->saveDir = $dir;
+  public function getFilePath($data) {
+    return $this->getSaveDir() ? $this->getSaveDir() . $this->getFileName($data) : NULL;
   }
-
 
 }
