cd7b49031319618567f0eb5c8161ff2b95ddef01
Shea McKinney <sherakama@gmail.com>
2015-06-10 11:36:14 -0700

    Changed issue collector to go to new Jira project

--- modules/capx_issue_collector/capx_issue_collector.js
+++ modules/capx_issue_collector/capx_issue_collector.js
@@ -7,7 +7,7 @@
     attach: function (context, settings) {
 
       $.ajax({
-        url: "https://stanfordits.atlassian.net/s/c64c585d847e1c97eda4c6f4b2a5ca4d-T/en_US879it1/64005/66/1.4.17/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=ca75a831",
+        url: "https://stanfordits.atlassian.net/s/7fbdb1a66f93c45a855a9c5e854450f9-T/en_US-ekvyw5/65001/66/1.4.25/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=d2cc7e6a",
         type: "get",
         cache: true,
         dataType: "script"
