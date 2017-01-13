9b7398e3da75b26f39663aefc847c3604cfe2b39
Sherakama <sherakama@gmail.com>
2017-01-13 10:09:03 -0800

    Create cctester.php

---
+++ cctester.php
@@ -0,0 +1,20 @@
+<?php
+
+class Foo {
+    public function bar() {
+        $name = $_POST['foo'];
+    }
+}
+
+class class_name {
+}
+
+class ClassName {
+    protected $property_name;
+    public function get_name() {
+    }
+    public function doSomething($user_name) {
+      $data_module = new DataModule();
+    }
+}
+
